0daf596307094ee7d48f3f7a4b8a4d89b5ec7d55
Bryan <bolivarbryan@gmail.com>
2015-05-18 13:27:42 -0500

    Edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,13 @@ def update
 	end
 end
 
+def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+
+	redirect_to root_path
+end
+
 	private 
 		def post_params
 			params.require(:post).permit(:title, :body)
